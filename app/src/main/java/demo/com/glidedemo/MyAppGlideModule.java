8eb36e731e82265be095b6ec31e91c1406cbf6e7
binhbt <binhbt@vega.com.vn>
2017-10-13 14:56:08 +0700

    init

---
+++ app/src/main/java/demo/com/glidedemo/MyAppGlideModule.java
@@ -0,0 +1,11 @@
+package demo.com.glidedemo;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by leobui on 10/13/2017.
+ */
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {}
